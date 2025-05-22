f77547fec0a76a096ce6ea8b2e0822413df9b892
mArOOu <marouanemebarekfalouti@gmail.com>
2025-05-22 17:12:34 +0100

    env and API reference update

--- src/index.js
+++ src/index.js
@@ -8,15 +8,14 @@ import exams from "../public/data/exams.json";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDh-_0h35V7Kz9Fi5TjDN7KnTjclkNKqZw",
-  authDomain: "qcmcalcbyhealthit.firebaseapp.com",
-  projectId: "qcmcalcbyhealthit",
-  storageBucket: "qcmcalcbyhealthit.firebasestorage.app",
-  messagingSenderId: "695183400619",
-  appId: "1:695183400619:web:c5ab737ad0f92d32a861ee",
-  measurementId: "G-411BFGX3V6",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDh-_0h35V7Kz9Fi5TjDN7KnTjclkNKqZw",
-  authDomain: "qcmcalcbyhealthit.firebaseapp.com",
-  projectId: "qcmcalcbyhealthit",
-  storageBucket: "qcmcalcbyhealthit.firebasestorage.app",
-  messagingSenderId: "695183400619",
-  appId: "1:695183400619:web:c5ab737ad0f92d32a861ee",
-  measurementId: "G-411BFGX3V6",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
